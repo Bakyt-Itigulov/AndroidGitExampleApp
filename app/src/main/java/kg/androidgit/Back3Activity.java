918f7e49faedd282d1a9c42f58732a6df932d53e
bakyt <bakyt480@gmail.com>
2021-09-10 15:00:16 +0600

    Добавил файлы

---
+++ app/src/main/java/kg/androidgit/Back3Activity.java
@@ -0,0 +1,19 @@
+package kg.androidgit;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Back3Activity extends AppCompatActivity {
+    private Context mContext = this;
+    private Bundle mExtras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}

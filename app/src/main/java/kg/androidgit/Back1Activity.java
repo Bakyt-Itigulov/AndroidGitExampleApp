97b458a77358c0cfb58841a52441ba8d0608541d
bakyt <bakyt480@gmail.com>
2021-09-10 14:57:16 +0600

    Удалили лишние файлы

---
+++ app/src/main/java/kg/androidgit/Back1Activity.java
@@ -0,0 +1,18 @@
+package kg.androidgit;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Back1Activity extends AppCompatActivity {
+    private Context mContext = this;
+    private Bundle mExtras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}

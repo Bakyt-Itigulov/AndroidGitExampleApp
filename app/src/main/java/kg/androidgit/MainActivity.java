7025459e0b1599cdcb8543937d90945f04ba5933
bakyt <bakyt480@gmail.com>
2021-09-10 15:16:59 +0600

    Добавил имя

--- app/src/main/java/kg/androidgit/MainActivity.java
+++ app/src/main/java/kg/androidgit/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     String[] mNames = {
             "Бакыт",
             "Асан",
-            "Усон"
+            "Усон",
+            "Иван"
     };
 
     @Override

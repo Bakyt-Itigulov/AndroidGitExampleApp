359b569b3e88c9607b245b462a5211c5565f4f65
Bakyt <bakyt@Bakyts-MacBook-Pro.local>
2021-09-10 14:39:03 +0600

    Initial commit

---
+++ app/src/main/java/kg/androidgit/MainActivity.java
@@ -0,0 +1,33 @@
+package kg.androidgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTextView;
+
+    String[] mNames = {
+            "Бакыт",
+            "Асан",
+            "Усон"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.text_view);
+
+        showNames();
+    }
+
+    private void showNames() {
+        String names = "";
+        for (int i = 0; i < mNames.length; i++) {
+            names += mNames[i] + "\n";
+        }
+
+        mTextView.setText(names);
+    }
+}

46599a47ad2ed66aa52b4efc47c9d66dad29a7f8
Perseverenсe <nurs.ryo@gmail.com>
2021-09-13 19:56:03 +0600

    FirstCommit

--- app/src/main/java/kg/androidgit/MainActivity.java
+++ app/src/main/java/kg/androidgit/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
             "Рома",
             "Иван",
             "Мирлан",
-            "Пепе"
+            "Пепе",
+            "Лоло"
     };
 
     @Override

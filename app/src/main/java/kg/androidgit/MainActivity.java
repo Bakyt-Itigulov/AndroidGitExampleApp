c51bbde0158a3f4c0d8eee8976892fa081f74687
tilek <@jffjdn>
2021-09-18 16:44:17 +0600

    new features

--- app/src/main/java/kg/androidgit/MainActivity.java
+++ app/src/main/java/kg/androidgit/MainActivity.java
@@ -2,34 +2,50 @@ package kg.androidgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button mBtn;
+    Button mDelete;
     TextView mTextView;
 
     String[] mNames = {
-            "Бакыт",
-            "Асан",
-            "Усон",
-            "Иван",
-            "Рома",
-            "Иван",
-            "Мирлан",
-            "Aйзирек",
-            "Кундуз",
-            "Мирлан",
-            "Усон",
-            "Не удаляйте пингвинов Лоло и Пепе"
+            "Бакыт","Бакыт","Бакыт","Бакыт","Бакыт",
+            "Бакыт","Бакыт","Бакыт","Бакыт","Бакыт",
+            "Бакыт","Бакыт","Бакыт","Бакыт","Бакыт",
+            "Бакыт","Бакыт","Бакыт","Бакыт","Бакыт",
+
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = findViewById(R.id.text_view);
 
-        showNames();
+        mTextView = findViewById(R.id.text_view);
+        mBtn = findViewById(R.id.show);
+        mDelete = findViewById(R.id.delete);
+
+
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNames();
+
+            }
+        });
+        mDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteNames();
+            }
+        });
+
+
     }
+
 
     private void showNames() {
         String names = "";
@@ -39,4 +55,11 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView.setText(names);
     }
+    public void deleteNames() {
+        String toNull = "";
+
+
+        mTextView.setText(toNull);
+    }
+
 }
